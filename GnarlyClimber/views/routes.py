bb694cc4ef9634be0353796d9f465afe08949ef0
Dennis Li <dennli@umich.edu>
2020-07-20 00:20:00 -0400

    dummy routes page renders on search submission

--- GnarlyClimber/views/routes.py
+++ GnarlyClimber/views/routes.py
@@ -1,10 +1,20 @@
 import flask
 import GnarlyClimber
+from flask import request
 
+@GnarlyClimber.app.route('/routes/', methods=["GET"])
+def show_routes():
+    '''
+    location = flask.request.form['location']
+    maxDistanceMiles = flask.request.form['maxDistanceMiles']
+    minDifficulty = flask.request.form['minDifficulty']
+    maxDifficulty = flask.request.form['maxDifficulty']
+    '''
+    location = request.args.get('location')
+    maxDistanceMiles = request.args.get('maxDistanceMiles')
+    minDifficulty = request.args.get('minDifficulty')
+    maxDifficulty = request.args.get('maxDifficulty')
 
-@GnarlyClimber.app.route('/routes/string:location>/<int:maxDistanceMiles>' \
-        '/<int:maxResults>/<string:minDifficulty>/<string:maxDifficulty>/', methods=["GET"])
-def show_routes(location, maxDistanceMiles, minDifficulty, maxDifficulty):
     """Display / route."""
     context = {
         "location" : location,
